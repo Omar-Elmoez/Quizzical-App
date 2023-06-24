c3fc49e0467e80d97f1bb87fc3991ac45f04bf67
Amr Fateem <amr.fateem@yahoo.com>
2023-06-24 18:29:22 +0300

    Amr Fix

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,26 +1,35 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
 import Answer from './Answer';
 
 export default function Question(props) {
+  const [selectedAnswer, setSelectedAnswer] = React.useState('');
 
-    const options = props.answers.map(item => {
-        return <Answer value={item} id={props.id} key={nanoid()} />
-    })
+  const handleAnswerSelect = (answerId) => {
+    setSelectedAnswer(answerId);
+  };
 
-    return (
-        <div className='px-5'>
-            <h2 className="font-karla font-bold text-base text-nileBlue">{props.content}</h2>
-            <div className='my-2.5 flex gap-4'>
-                {options}
-            </div>
-            <hr className='mb-2.5' />
-        </div>
-    );
+  const options = props.answers.map((item, index) => (
+    <Answer
+      value={item}
+      id={`answer_${props.id}_${index}`}
+      key={`answer_${props.id}_${index}`}
+      isSelected={selectedAnswer === `answer_${props.id}_${index}`}
+      onSelect={handleAnswerSelect}
+    />
+  ));
+
+  return (
+    <div className="px-5">
+      <h2 className="font-karla font-bold text-base text-nileBlue">{props.content}</h2>
+      <div className="my-2.5 flex gap-4">{options}</div>
+      <hr className="mb-2.5" />
+    </div>
+  );
 }
 
 Question.propTypes = {
-    content: PropTypes.string,
-    answers: PropTypes.array,
-    id: PropTypes.string,
-}
+  content: PropTypes.string,
+  answers: PropTypes.array,
+  id: PropTypes.string,
+};
