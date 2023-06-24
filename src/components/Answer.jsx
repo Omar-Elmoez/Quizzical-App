1dbdc63c4f145074f505639ea34b77d628aa49dc
Omar Elmoez <60140264+Omar-Elmoez@users.noreply.github.com>
2023-06-24 19:00:29 +0300

    Revert "Amr Fix"

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,36 +1,24 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { decode } from 'html-entities';
+import {decode} from 'html-entities';
+
 
 export default function Answer(props) {
-  const handleClick = () => {
-    props.onSelect(props.id);
-  };
 
-  return (
-    <label
-      className={`text-center rounded-lg p-1 w-[calc(90%_/_4)] border-Twilight border ${
-        props.isSelected ? 'border-white bg-Geyser' : ''
-      } flex justify-center items-center relative`}
-    >
-      <input
-        type="radio"
-        name={props.name}
-        value={decode(props.value)}
-        className="absolute inset-0 cursor-pointer opacity-0"
-        checked={props.isSelected}
-        onChange={handleClick}
-      />
-      <span className="font-Inter font-medium text-nileBlue text-sm">
-        {decode(props.value)}
-      </span>
-    </label>
-  );
+    const[isClicked, setIsClicked] = React.useState(false);
+    function handleClick() {
+        setIsClicked(prevState => !prevState)
+    }
+
+    return (
+        <div className={`text-center rounded-lg p-1 w-[calc(90%_/_4)] border-Twilight border ${isClicked && "border-white bg-Geyser"} flex justify-center items-center relative`}>
+            <input type="radio" name={props.id} value={decode(props.value)} className={`absolute inset-0 cursor-pointer opacity-0`} onClick={handleClick} />
+            <label className='font-Inter font-medium text-nileBlue text-sm'>{decode(props.value)}</label>
+        </div>
+    );
 }
 
 Answer.propTypes = {
-  value: PropTypes.string,
-  id: PropTypes.string,
-  name: PropTypes.string,
-  isSelected: PropTypes.bool,
-  onSelect: PropTypes.func,
-};
+    value: PropTypes.string,
+    id: PropTypes.string,
+}
